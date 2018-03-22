18eae4622231260f5a9ae895b8963543780a9c6c
Paola David <Andree709@hotmail.com>
2018-03-22 07:24:28 -0500

    se ajustan interfaces

--- app/src/main/java/co/edu/usco/uvindexkt/MainActivity.kt
+++ app/src/main/java/co/edu/usco/uvindexkt/MainActivity.kt
@@ -3,15 +3,16 @@ package co.edu.usco.uvindexkt
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
+import android.support.transition.TransitionManager
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main_full.*
 
 class MainActivity : AppCompatActivity() {
 
-    val fullSet:ConstraintSet = ConstraintSet()
-    val mainSet:ConstraintSet = ConstraintSet()
-    val notifiSet:ConstraintSet = ConstraintSet()
-    var initial:Boolean = true
+    val fullSet: ConstraintSet = ConstraintSet()
+    val mainSet: ConstraintSet = ConstraintSet()
+    val notifiSet: ConstraintSet = ConstraintSet()
+    var initial: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,15 +24,38 @@ class MainActivity : AppCompatActivity() {
         notifiSet.clone(this, R.layout.activity_main_notifi)
         fullSet.clone(root)
 
-        floaArrow.setOnClickListener{
-            if(floaNoti.visibility==View.VISIBLE) {
+        floaArrow.setOnClickListener {
+            TransitionManager.beginDelayedTransition(root)
+            if (floaNoti.visibility == View.VISIBLE) {
+
                 floaNoti.visibility = View.GONE
                 floaInf.visibility = View.GONE
-            }else{
+            } else {
+
                 floaNoti.visibility = View.VISIBLE
                 floaInf.visibility = View.VISIBLE
             }
 
         }
+        floaNoti.setOnClickListener{
+            TransitionManager.beginDelayedTransition(root)
+            if(initial) notifiSet.applyTo(root)
+            else fullSet.applyTo(root)
+
+            println(initial)
+
+            initial=!initial
+
+        }
+
+        floaInf.setOnClickListener {
+            TransitionManager.beginDelayedTransition(root)
+            if(initial) mainSet.applyTo(root)
+            else fullSet.applyTo(root)
+
+
+            initial=!initial
+        }
+
     }
 }
